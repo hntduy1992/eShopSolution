3d2e84af4e3b9acf618c054fee2a0fc9ec021d18
duy ho <hntduy1992@gmail.com>
2021-09-09 11:35:43 +0700

    Add Identity Database

---
+++ eShopSolution.Data/Migrations/20210909043426_seedIdentityDatabase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class seedIdentityDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 9, 11, 34, 25, 402, DateTimeKind.Local).AddTicks(4489),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 9, 9, 11, 26, 54, 739, DateTimeKind.Local).AddTicks(273));
+
+            migrationBuilder.InsertData(
+                table: "AppRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
+                values: new object[] { new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"), "cd0dcba6-a208-438e-b901-d1b259248f2d", "Administrator role", "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AppUserRoles",
+                columns: new[] { "UserId", "RoleId" },
+                values: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), new Guid("8d04dce2-969a-435d-bba4-df3f325983dc") });
+
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Dob", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), 0, "dc0a79e3-a891-4ff0-8aad-f648210bc229", new DateTime(2020, 1, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "hntduy1992@gmail.com", true, "Duy", "Hồ", false, null, "hntduy1992@gmail.com", "admin", "AQAAAAEAACcQAAAAEFKDDJb5IJut56lM6ch4/MvgeBaijoleTiIJid0P1VKact1NjJ9iOGY2KfY9iIoTkA==", null, false, "", false, "admin" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 9, 9, 11, 34, 25, 416, DateTimeKind.Local).AddTicks(5236));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"));
+
+            migrationBuilder.DeleteData(
+                table: "AppUserRoles",
+                keyColumns: new[] { "UserId", "RoleId" },
+                keyValues: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), new Guid("8d04dce2-969a-435d-bba4-df3f325983dc") });
+
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 9, 11, 26, 54, 739, DateTimeKind.Local).AddTicks(273),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2021, 9, 9, 11, 34, 25, 402, DateTimeKind.Local).AddTicks(4489));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 9, 9, 11, 26, 54, 752, DateTimeKind.Local).AddTicks(1998));
+        }
+    }
+}
